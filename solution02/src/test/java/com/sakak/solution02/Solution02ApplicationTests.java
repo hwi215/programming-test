c9e9592b5b0f48793e28ba3637d7cf0cd34f2767
hwi215 <buc06137@naver.com>
2023-10-14 16:18:15 +0900

    Init

---
+++ solution02/src/test/java/com/sakak/solution02/Solution02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakak.solution02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Solution02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
