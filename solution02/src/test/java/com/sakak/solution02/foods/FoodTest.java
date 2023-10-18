f8ce3a8217cfc875002704ba032a67a3b888e86d
hwi215 <buc06137@naver.com>
2023-10-18 19:34:01 +0900

    Test: solution02 음식 정보 조회 검증하는 실패 테스트 코드 작성

---
+++ solution02/src/test/java/com/sakak/solution02/foods/FoodTest.java
@@ -0,0 +1,37 @@
+package com.sakak.solution02.foods;
+import com.sakak.solution02.food.dto.RequestFoodDto;
+import com.sakak.solution02.food.dto.ResponseFoodDto;
+import com.sakak.solution02.food.service.FoodService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import javax.transaction.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+public class FoodTest {
+    @Autowired
+    FoodService ms;
+
+    @Test
+    @Transactional
+    @Rollback
+    @DisplayName("조회 테스트")
+    public void foodListTest() {
+
+        RequestFoodDto requestFoodDto = new RequestFoodDto("닭갈비", "2019","춘천","D000008");
+
+        List<ResponseFoodDto> responseFoodDtoList = ms.getFoodList(requestFoodDto);
+
+        // 3. List의 size()가 1인지 비교.
+        // size()가 1이면 테스트 통과
+        assertThat(responseFoodDtoList.size()).isEqualTo(1);
+
+    }
+}
