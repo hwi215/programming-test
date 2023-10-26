44c13b327275957ee0296b488e4ac58f6b583ef0
hwi215 <buc06137@naver.com>
2023-10-26 15:49:31 +0900

    Test: solution02 Get Method API 실패 테스트(MockMvc 사용)

--- solution02/src/test/java/com/sakak/solution02/foods/FoodTest.java
+++ solution02/src/test/java/com/sakak/solution02/foods/FoodTest.java
@@ -27,6 +27,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.Map;
 
@@ -44,22 +45,39 @@ public class FoodTest {
     private MockMvc mockMvc;
 
     @Test
-    @DisplayName("Get Method Test")
-    void getTest() throws Exception {
+    @DisplayName("음식 정보 조회 Get Method Test")
+    public void 음식_정보_조회_GET() throws Exception {
         // api 요청
         mockMvc
                 .perform(
                         get("/foods") // url
-                                .param("food_name", "김치국") // parameter 1
+                                .param("food_name", "닭갈비") // parameter 1
                                 .param("research_year", "2019") // parameter 2
                                 .param("maker_name", "전국(대표)") // parameter 3
-                                .param("food_code", "D000032") // parameter 4
+                                .param("food_code", "D000007") // parameter 4
                 )
                 .andDo(print()) // api 수행내역 로그 출력
                 .andExpect(status().isOk()) // response status 200 검증
                 .andExpect(jsonPath("code").value("200")) // response code 데이터 검증
-                .andExpect(jsonPath("message").value("success")) // response message 데이터 검증
-                .andDo(print());
+                .andExpect(jsonPath("message").value("success")); // response message 데이터 검증
+
+    }
+
+
+    @Test
+    @DisplayName("음식 정보 조회 실패 Get Method Test")
+    public void 음식_정보_조회_실패_GET() throws Exception {
+        // api 요청
+        mockMvc
+                .perform(
+                        get("/foods") // url
+                                .param("food_name", "닭갈비2") // parameter 1
+                                .param("research_year", "2019") // parameter 2
+                                .param("maker_name", "전국(대표)") // parameter 3
+                                .param("food_code", "D000007") // parameter 4
+                )
+                .andDo(print()) // api 수행내역 로그 출력
+                .andExpect(status().is4xxClientError()); // 404 not found error 발생하는 지 검증
     }
     /*
     @Autowired
